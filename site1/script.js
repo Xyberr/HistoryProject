e55cae39fb85363550298e3dd8f1f8a0ec4a768c
Xyberr <makszacev238@gmail.com>
2023-12-18 14:46:22 +0500

    add quiz script

--- site1/script.js
+++ site1/script.js
@@ -5,3 +5,102 @@ function speak(text) {
     message.text = text;
     window.speechSynthesis.speak(message)
 }
+
+const quizData = [
+    {
+        question: "Что делать если спиздил код с гитхаба?",
+        a: "Выйти в окно",
+        b: "Выпрыгнуть в окно",
+        c: "Отпиздить тех кто нихуя не делает",
+        correct: "c",
+    },
+    {
+        question: "What does CSS stand for?",
+        a: "Cascading Style Sheets",
+        b: "Cars SUVs Sailboats",
+        c: "Central Style Sheets",
+        correct: "a",
+    },
+    {
+        question: "What year did JavaScript appear?",
+        a: "1899",
+        b: "2005",
+        c: "1995",
+        correct: "c",
+    },
+    {
+        question: "Who is the author of the JavaScript language?",
+        a: "Bill Gates",
+        b: "Brendan Eich",
+        c: "Steve Jobs",
+        correct: "b",
+    },
+    {
+        question: "What language is this project written in?",
+        a: "JavaScript",
+        b: "Ruby",
+        c: "C++",
+        correct: "a",
+    },
+];
+
+const quiz = document.getElementById('quiz');
+const answerElements = document.querySelectorAll('.answer');
+const questionElement = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const submit = document.getElementById('submit');
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz(){
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+
+    questionElement.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+}
+
+function deselectAnswers(){
+    answerElements.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected(){
+    let answer;
+
+    answerElements.forEach(answerEl => {
+        if(answerEl.checked){
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+submit.addEventListener('click', () => {
+    const answer = getSelected();
+
+    if(answer){
+        if(answer === quizData[currentQuiz].correct){
+            score++;
+        }
+
+        currentQuiz++;
+
+        if(currentQuiz < quizData.length){
+            loadQuiz();
+        }
+        else{
+            quiz.innerHTML = `<h2>You answered coreectly at ${score}/${quizData.length} questions</h2>
+            <button onclick="location.reload()">Reload</button>
+            `;
+        }
+    }
+});
